00f8033839d19e7734fd8e749e745abcd924a3ed
Kirill Shishkov <93086215+CCCProo@users.noreply.github.com>
2022-09-22 15:31:50 +0300

    Update init-oauth.php

--- src/init-oauth.php
+++ src/init-oauth.php
@@ -1,6 +1,6 @@
 <?php
 
-$discord_url = "{REDIRECT_URL_LINK_FROM_DASHBOARD}";
+$discord_url = "{https://discord.com/api/oauth2/authorize?client_id=1022469036673618000&redirect_uri=https%3A%2F%2Fcccproo.github.io%2Findex.html&response_type=code&scope=identify}";
 header("Location: $discord_url");
 exit();
 
